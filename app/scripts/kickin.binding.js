fb72ba50be8ce85a48ebd5241ec767995dd32b03
holger <holger@surfacepattern.ch>
2014-06-09 21:12:11 +0200

    Chaotic wip, templates with loop!

---
+++ app/scripts/kickin.binding.js
@@ -0,0 +1,18 @@
+/**
+ * Created by holger on 09.06.2014.
+ */
+ki.namespace('ki.binding');
+
+ki.binding = (function() {
+
+    var BINDING_ATTRIBUTE = 'data-ki-bind';
+
+    return {
+        /**
+         * This function enhances a given object with additional methods
+         */
+        enhance: function(baseObject, enhanceObject) {
+            // TODO implement
+        }
+    }
+})();
